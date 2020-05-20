a8806b2cf4c6d3e6cd1ea275432f2a4803a84f40
Karl Waclawek <karl@waclawek.net>
2020-05-19 21:37:58 -0400

    WebClient: gettext.js update, removed postcss from dependencies, we only have dev dependencies on postcss now.

--- EtwEvents.WebClient/postcss_prebuild.cmd
+++
@@ -1,4 +0,0 @@
-
-node node_modules/postcss-cli/bin/postcss src/css/fontawesome/css/all.css -o wwwroot/css/fontawesome/css/all.css
-
-node node_modules/postcss-cli/bin/postcss src/css/**/*.pcss --dir wwwroot/css --ext css
