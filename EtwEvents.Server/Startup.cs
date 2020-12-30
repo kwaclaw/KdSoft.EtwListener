be5feee0c99194af3f7b8c8c402d0da07ef03548
Karl Waclawek <karl@waclawek.net>
2020-12-29 19:33:00 -0500

    Server: added file logging.

--- EtwEvents.Server/Startup.cs
+++ EtwEvents.Server/Startup.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 #pragma warning disable CA1822 // Mark members as static
 #pragma warning disable CA1801 // Review unused parameters
@@ -17,6 +20,18 @@ namespace KdSoft.EtwEvents.Server
         }
 
         public IConfiguration Configuration { get; }
+
+        // fix up log file base name to become a template for date-based log files
+        string MakeLogFileTemplate(string logFilePath) {
+            var logDir = Path.GetDirectoryName(logFilePath);
+            if (string.IsNullOrEmpty(logDir))
+                logDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? "";
+            var baseFileName = Path.GetFileNameWithoutExtension(logFilePath);
+            var ext = Path.GetExtension(logFilePath);
+            var logFileNameTemplate = $"{baseFileName}_{{0:yyyy}}-{{0:MM}}-{{0:dd}}{ext}";
+            var logFilePathTemplate = Path.Combine(logDir, logFileNameTemplate);
+            return logFilePathTemplate;
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
@@ -61,6 +76,26 @@ namespace KdSoft.EtwEvents.Server
             });
 
             services.AddSingleton<TraceSessionManager>(provider => new TraceSessionManager(TimeSpan.FromMinutes(3)));
+
+            services.AddLogging(loggingBuilder => {
+                var loggingSection = Configuration.GetSection("Logging");
+                // fix up log file path to become a template for date-based log files
+                var logFilePathTemplate = MakeLogFileTemplate(loggingSection["File:Path"]);
+                loggingSection["File:Path"] = logFilePathTemplate;
+
+                loggingBuilder.AddFile(loggingSection, loggerOpts => {
+                    var startDate = DateTimeOffset.MinValue.Date;
+                    var logFileName = String.Format(logFilePathTemplate, startDate);
+                    loggerOpts.FormatLogFileName = logFileTemplate => {
+                        var today = DateTimeOffset.Now.Date;
+                        if (today != startDate) {
+                            startDate = today;
+                            logFileName = String.Format(logFileTemplate, today);
+                        }
+                        return logFileName;
+                    };
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
