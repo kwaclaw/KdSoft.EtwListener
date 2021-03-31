210871d8cc4aa92a3b00214be8226f740960cc25
Karl Waclawek <karl@waclawek.net>
2021-03-30 20:01:07 -0400

    EtwEvents.AgentManager - import updates from WebClient project.

--- EtwEvents.AgentManager/wwwroot/js/utils.js
+++ EtwEvents.AgentManager/wwwroot/js/utils.js
@@ -57,78 +57,113 @@ export const dateFormat = new Intl.DateTimeFormat('default', {
   milli: 'numeric'
 });
 
-// const noType = 0;
-// const arrayType = 1;
-// const objectType = 2;
-
-// function typeOf(input) {
-//   if (Array.isArray(input)) return arrayType;
-//   if (input !== null && input instanceof Object) return objectType;
-//   return noType;
-// }
-
-// function clone(input) {
-//   let output = input;
-//   const type = typeOf(input);
-//   if (type === arrayType) {
-//     output = [];
-//     const size = input.length;
-//     for (let index = 0; index < size; index += 1) {
-//       output[index] = clone(input[index]);
-//     }
-//   } else if (type === objectType) {
-//     output = {};
-//     for (const key in input) {
-//       if (!Object.prototype.hasOwnProperty.call(input, key)) continue;
-//       output[key] = clone(input[key]);
-//     }
-//   }
-//   return output;
-// }
-
-// // first argument must be boolean - true for deep clone, false for shallow
-// export function mergeObjects(...args) {
-//   let result = args[0];
-//   const deep = (result === true);
-//   const size = arguments.length;
-//   if (deep || typeOf(result) !== objectType) result = {};
-
-//   for (let index = 1; index < size; index += 1) {
-//     const item = args[index];
-//     if (typeOf(item) === objectType) {
-//       for (const key in item) {
-//         if (!Object.prototype.hasOwnProperty.call(item, key)) continue;
-//         result[key] = deep ? clone(item[key]) : item[key];
-//       }
-//     }
-//   }
-//   return result;
-// }
-
-// this performs a full clone of a Javascript object or array
-export function cloneObject(target, source) {
-  // eslint-disable-next-line guard-for-in
-  for (const key in source) {
-    // Use getOwnPropertyDescriptor instead of source[key] to avoid triggering setter/getter.
-    const descriptor = Object.getOwnPropertyDescriptor(source, key);
-    if (descriptor.value instanceof String) {
-      target[key] = descriptor.value.slice(0);
-    } else if (descriptor.value instanceof Array) {
-      target[key] = cloneObject([], descriptor.value);
-    } else if (descriptor.value instanceof Function) {
-      Object.defineProperty(target, key, descriptor);
-    } else if (descriptor.value instanceof Object) {
-      const prototype = Reflect.getPrototypeOf(descriptor.value);
-      const cloneObj = cloneObject({}, descriptor.value);
-      Reflect.setPrototypeOf(cloneObj, prototype);
-      target[key] = cloneObj;
-    } else {
+// this performs a deep (resursive) clone of a Javascript Array, Map, Set, Date or Object;
+// other element or property types are assumed to be primitive types like Number or Boolean;
+// it should be sufficient for data transfer objects and view models
+export function clone(source) {
+  if (source instanceof Array) {
+    const tgtArray = new Array(source.length);
+    for (let indx = 0; indx < source.length; indx += 1) {
+      tgtArray[indx] = clone(source[indx]);
+    }
+    return tgtArray;
+  }
+  if (source instanceof Map) {
+    const tgtMap = new Map();
+    for (const [key, value] of source) {
+      tgtMap.set(clone(key), clone(value));
+    }
+    return tgtMap;
+  }
+  if (source instanceof Set) {
+    const tgtSet = new Set();
+    for (const element of source) {
+      tgtSet.add(clone(element));
+    }
+    return tgtSet;
+  }
+  if (source instanceof String) {
+    return source.slice(0);
+  }
+  if (source instanceof Date) {
+    return new Date(+source);
+  }
+  if (source instanceof Object) {
+    const target = {};
+    // eslint-disable-next-line guard-for-in
+    for (const key in source) {
+      // Use getOwnPropertyDescriptor instead of source[key] to avoid triggering setter/getter.
+      const descriptor = Object.getOwnPropertyDescriptor(source, key);
+      const clonedValue = clone(descriptor.value);
+      descriptor.value = clonedValue;
       Object.defineProperty(target, key, descriptor);
     }
+    const prototype = Reflect.getPrototypeOf(source);
+    Reflect.setPrototypeOf(target, prototype);
+    return target;
   }
-  const prototype = Reflect.getPrototypeOf(source);
-  Reflect.setPrototypeOf(target, prototype);
-  return target;
+  // assume everything else is a primitive type (number, boolean, ...)
+  return source;
+}
+
+// this compares the target's properties to the same properties in source;
+// extra properties in source objects are ignored, but not in Arrays, Maps or Sets
+export function targetEquals(target, source) {
+  if (target instanceof Array) {
+    if (source instanceof Array && source.length === target.length) {
+      for (let indx = 0; indx < target.length; indx += 1) {
+        if (!targetEquals(target[indx], source[indx])) {
+          return false;
+        }
+      }
+    } else {
+      return false;
+    }
+    return true;
+  }
+  if (target instanceof Map) {
+    if (source instanceof Map && source.size === target.size) {
+      for (const [key, value] of target) {
+        if (!targetEquals(value, source.get(key))) {
+          return false;
+        }
+      }
+    } else {
+      return false;
+    }
+    return true;
+  }
+  if (target instanceof Set) {
+    if (source instanceof Set && source.size === target.size) {
+      for (const element of target) {
+        if (!source.has(element)) {
+          return false;
+        }
+      }
+    } else {
+      return false;
+    }
+    return true;
+  }
+  if (target instanceof Date) {
+    if (source instanceof Date) {
+      if (Number(target) !== Number(source)) {
+        return false;
+      }
+    }
+    return false;
+  }
+  if (target instanceof Object) {
+    // eslint-disable-next-line guard-for-in
+    for (const key in target) {
+      if (!targetEquals(target[key], source[key])) {
+        return false;
+      }
+    }
+    return true;
+  }
+  // compare as primitive types
+  return target === source;
 }
 
 // this assigns existing target object properties from source properties of the same name;
