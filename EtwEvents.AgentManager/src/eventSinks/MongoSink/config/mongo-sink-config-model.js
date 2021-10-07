e1a08c0e1c0f8f16898cfb0e0dbf4d35e01f1bef
Karl Waclawek <karl@waclawek.net>
2021-10-07 15:45:30 -0400

    AgentManager, WebClient: replaced RoboCopy command with cross-platform MSBuild task.

---
+++ EtwEvents.AgentManager/src/eventSinks/MongoSink/config/mongo-sink-config-model.js
@@ -0,0 +1,35 @@
+﻿/* global i18n */
+
+class MongoSinkConfigModel {
+  constructor(name, sinkType) {
+    this.name = name;
+    this.sinkType = sinkType;
+    this.options = {
+      origin: 'mongodb://',
+      replicaset: '',
+      database: '',
+      collection: '',
+      eventFilterFields: ['Timestamp', 'ProviderName', 'Id', 'Level', 'Keywords', 'Opcode', 'TaskName'],
+      payloadFilterFields: []
+    };
+    this.credentials = {
+      database: '',
+      user: '',
+      password: '',
+      certificateCommonName: ''
+    };
+  }
+
+  static get eventFields() {
+    // if intended to be the source for a KdSoftChecklistModel, then the entries must be objects
+    return [{ id: 'Timestamp' }, { id: 'ProviderName' }, { id: 'Channel' }, { id: 'Id' }, { id: 'Level' }, { id: 'Keywords' },
+    { id: 'Opcode' }, { id: 'OpcodeName' }, { id: 'TaskName' }, { id: 'Version' }
+    ];
+  }
+
+  export() {
+    //
+  }
+}
+
+export default MongoSinkConfigModel;
---
+++ EtwEvents.AgentManager/src/eventSinks/SeqSink/config/seq-sink-config-model.js
@@ -0,0 +1,21 @@
+﻿/* global i18n */
+
+class SeqSinkConfigModel {
+  constructor(name, sinkType) {
+    this.name = name;
+    this.sinkType = sinkType;
+    this.options = {
+      serverUrl: '',
+      proxyAddress: '',
+    };
+    this.credentials = {
+      apiKey: '',
+    };
+  }
+
+  export() {
+    //
+  }
+}
+
+export default SeqSinkConfigModel;
