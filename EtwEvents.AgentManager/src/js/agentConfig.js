c3d053a8a03b7ecc1c0697859564b95264d3e1f5
Karl Waclawek <karl@waclawek.net>
2021-10-24 19:30:09 -0400

    AgentManager: added batchSize and maxWriteDelayMSecs to agentConfig.js.

--- EtwEvents.AgentManager/src/js/agentConfig.js
+++ EtwEvents.AgentManager/src/js/agentConfig.js
@@ -1,10 +1,12 @@
 ﻿class AgentConfig {
-  constructor(id, site, host, enabledProviders, eventSink, filterBody) {
+  constructor(id, site, host, enabledProviders, eventSink, batchSize, maxWriteDelayMSecs, filterBody) {
     this.id = id || 'id';
     this.site = site || 'Site';
     this.host = host || 'Host';
     this.enabledProviders = enabledProviders || [];
     this.eventSink = eventSink || {};
+    this.batchSize = batchSize || 100;
+    this.maxWriteDelayMSecs = maxWriteDelayMSecs || 400;
     this.filterBody = filterBody || 'return true;';
   }
 }
