fd5461372eff10851264dad855a15008105ed64f
Karl Waclawek <karl@waclawek.net>
2021-11-14 19:00:31 -0500

    PushAgent: logging base exceptions.

--- EtwEvents.PushAgent/Utils.cs
+++ EtwEvents.PushAgent/Utils.cs
@@ -5,16 +5,22 @@ namespace KdSoft.EtwEvents.PushAgent
 {
     public static class Utils
     {
+        /// <summary>
+        /// Logs exception drilling down into inner exceptions and base exceptions.
+        /// </summary>
+        /// <param name="logger">Logger instance.</param>
+        /// <param name="ex">Exception to log.</param>
+        /// <param name="message">Error message.</param>
         public static void LogAllErrors(this ILogger logger, Exception ex, string? message = null) {
             if (logger == null)
                 return;
             if (ex is AggregateException aggex) {
                 foreach (var iex in aggex.Flatten().InnerExceptions) {
-                    logger.LogError(iex, message ?? iex.Message);
+                    logger.LogError(iex.GetBaseException(), message ?? iex.Message);
                 }
             }
             else {
-                logger.LogError(ex, message ?? ex.Message);
+                logger.LogError(ex.GetBaseException(), message ?? ex.Message);
             }
         }
     }
