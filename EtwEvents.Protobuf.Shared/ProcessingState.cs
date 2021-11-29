e17f5c0cd03ed8f5bee7e38f20b06922c38904c6
Karl Waclawek <karl@waclawek.net>
2021-11-29 00:26:23 -0500

    Protobuf.Shared: the default for ProcessingState.FilterSource should be null, to indicate absence of a filter.

--- EtwEvents.Protobuf.Shared/ProcessingState.cs
+++ EtwEvents.Protobuf.Shared/ProcessingState.cs
@@ -3,7 +3,6 @@
     partial class ProcessingState
     {
         partial void OnConstruction() {
-            this.FilterSource = new FilterSource();
             this.BatchSize = 100;
             this.MaxWriteDelayMSecs = 400;
         }
