55f79b5fc17374fb483b3086a129738a9675bd43
Karl Waclawek <karl@waclawek.net>
2022-01-16 00:29:40 -0500

    EtwEvents.SpaDevServer - added console app that starts the "vite" dev server, checking if it is already running by testing the port.

---
+++ EtwEvents.SpaDevServer/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+
+var workingDir = Path.GetFullPath("..\\..\\..\\..\\EtwEvents.AgentManager\\Spa");
+
+var ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+var tcpConnInfoArray = ipGlobalProperties.GetActiveTcpConnections();
+
+bool isAvailable = true;
+foreach (var tcpi in tcpConnInfoArray) {
+    if (tcpi.LocalEndPoint.Port == 41000) {
+        isAvailable = false;
+        break;
+    }
+}
+if (!isAvailable) {
+    return;
+}
+
+var startInfo = new ProcessStartInfo {
+    UseShellExecute = true,
+    CreateNoWindow = false,
+    FileName = "npm",
+    Arguments = "run dev",
+    WorkingDirectory = workingDir
+};
+var process = Process.Start(startInfo);
