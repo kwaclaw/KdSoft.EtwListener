d722e29bd82dc8af637914d84fb7d988d9f57644
Karl Waclawek <karl@waclawek.net>
2022-01-21 21:18:57 -0500

    Shared: added second ValueHolder class, for two values.

--- EtwEvents.Shared/ValueHolder.cs
+++ EtwEvents.Shared/ValueHolder.cs
@@ -3,5 +3,22 @@
     public class ValueHolder<T> where T: notnull, new()
     {
         public T Value { get; set; } = new T();
+
+        public void Reset() {
+            Value = new T();
+        }
+    }
+
+    public class ValueHolder<T, V>
+        where T: notnull, new()
+        where V: notnull, new()
+    {
+        public T Value1 { get; set; } = new T();
+        public V Value2 { get; set; } = new V();
+
+        public void Reset() {
+            Value1 = new T();
+            Value2 = new V();
+        }
     }
 }
