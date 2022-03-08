dd3b0aecc3c223c4c2a76ebf7c62fd797560dd69
Karl Waclawek <karl@waclawek.net>
2022-03-07 23:27:15 -0500

    PushAgent: cleanup, error handling in MakeKdSoftClientCert.cmd.

--- EtwEvents.PushAgent/certificates/MakeKdSoftClientCert.cmd
+++ EtwEvents.PushAgent/certificates/MakeKdSoftClientCert.cmd
@@ -1,16 +1,15 @@
-rem @echo off
+@echo off
 Setlocal enabledelayedexpansion
 
 rem Example: MakeKdSoftClientCert.cmd -name my-etw-site -email karl@kd-soft.net -extra "role^=etw-pushagent"
 
-rem Common Name (CN), default empty
+rem Common Name (CN)
 set -name=
-rem email address, default empty
+rem email address
 set -email=
-rem extra DN component, to be added at end of DN, default empty
+rem extra DN component, to be added at end of DN
 rem should be in double quotes, any '=' must be escaped as '^='
 set -extra=
-
 
 rem %~1 removes surrounding quotes from first parameter
 for %%a in (%*) do (
@@ -24,18 +23,20 @@ if [%-extra%] == [] ( cd. ) else (set -extra=/%-extra%)
 
 set cn=CN=%-name%
 set em=/emailAddress=%-email%
-
 set dn=/C=CA/ST=ON/L=Oshawa/O=Kd-Soft/%cn%%em%%-extra%
 
 @echo generate client key
 openssl genrsa -out "tmp/client.key" 4096
+if %ERRORLEVEL% NEQ 0 (Exit /b)
 
 @echo generate CSR
 openssl req -new -key "tmp/client.key" -out "tmp/client.csr" -sha256 -config "kd-soft-client.cnf"
+if %ERRORLEVEL% NEQ 0 (Exit /b)
     
 @echo create and sign the client certificate    
 openssl x509 -req -days 750 -in "tmp/client.csr" -sha256 -CA "Kd-Soft.crt" -CAkey "Kd-Soft.key" ^
     -CAcreateserial -out "tmp/client.crt" -subj "%dn%" -copy_extensions copy
+if %ERRORLEVEL% NEQ 0 (Exit /b)
     
 @echo export to pkcs12
 openssl pkcs12 -export -in "tmp/client.crt" -inkey "tmp/client.key" -out "out/%-name%.p12"
