d1bd930861461e0010d6f37769363d6178fded6d
Karl Waclawek <karl@waclawek.net>
2022-03-20 19:41:04 -0400

    AgentManager: create certificates usable by Server 2016 / Win10 1703.

--- EtwEvents.AgentManager/certificates/MakeKdSoftServerCert.cmd
+++ EtwEvents.AgentManager/certificates/MakeKdSoftServerCert.cmd
@@ -26,16 +26,17 @@ openssl genrsa -out "tmp/server.key" 4096
 if %ERRORLEVEL% NEQ 0 (Exit /b)
 
 @echo generate CSR
-openssl req -new -key "tmp/server.key" -out "tmp/server.csr" -sha256 -config "kd-soft-server.cnf" -addext "subjectAltName=DNS:%-dns%"
+openssl req -new -key "tmp/server.key" -out "tmp/server.csr" -config "kd-soft-server.cnf" -addext "subjectAltName=DNS:%-dns%"
 if %ERRORLEVEL% NEQ 0 (Exit /b)
     
 @echo create and sign the server certificate    
-openssl x509 -req -days 398 -in "tmp/server.csr" -sha256 -CA "Kd-Soft.crt" -CAkey "Kd-Soft.key" ^
+openssl x509 -req -days 398 -in "tmp/server.csr" -CA "Kd-Soft.crt" -CAkey "Kd-Soft.key" ^
     -CAcreateserial -out "tmp/server.crt" -subj "%dn%" -copy_extensions copy
 if %ERRORLEVEL% NEQ 0 (Exit /b)
     
 @echo export to pkcs12
-openssl pkcs12 -export -in "tmp/server.crt" -inkey "tmp/server.key" -out "out/%-dns%.p12"
-
-    
+rem sha256 encrypted keys cannot be imported on WinServer 2016 / Win10 <= 1703, use TripleDES-SHA1 instead
+rem openssl pkcs12 -export -in "tmp/server.crt" -inkey "tmp/server.key" -out "out/%-dns%.p12"
+openssl pkcs12 -export -in "tmp/server.crt" -inkey "tmp/server.key" -macalg SHA1 -keypbe PBE-SHA1-3DES -certpbe PBE-SHA1-3DES -out "out/%-dns%.p12"
+   
 pause
