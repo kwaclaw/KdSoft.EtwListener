66fa20a8f868f2760d9f084ecea0d51d0f3a42b5
Karl Waclawek <karl@waclawek.net>
2022-03-23 01:21:46 -0400

    AgentManager: Its a server certificate.

--- EtwEvents.AgentManager/deploy/tools/InstallServerPfx.cmd
+++ EtwEvents.AgentManager/deploy/tools/InstallServerPfx.cmd
@@ -3,7 +3,7 @@
 REM switch to this batch file's directory (needed when running as administrator)
 pushd "%~dp0"
 
-@echo Installing KdSoft ETW Push Agent client certificate...
+@echo Installing KdSoft ETW Agent Manager server certificate...
 
 elevate -wait4exit certutil.exe -importpfx "%1"
 
