bfadea84370ed8247cb8a45c75f584d60e9a4d34
Karl Waclawek <karl@waclawek.net>
2022-04-16 23:55:00 -0400

    Shared: added RetryStartTimeMSecs field to EventSinkStatus, for easier use by Javascript date handling.

---
+++ EtwEvents.Protobuf.Shared/EventSinkStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace KdSoft.EtwLogging
+{
+    partial class EventSinkStatus
+    {
+        partial void OnConstruction() {
+            //
+        }
+
+        public long? RetryStartTimeMSecs => (RetryStartTime == null) ? null : (RetryStartTime.Seconds * 1000) + (RetryStartTime.Nanos / 1000000);
+    }
+}
