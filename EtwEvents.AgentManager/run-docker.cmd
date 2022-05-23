8c7d03192337107ebcf1ddb2da1f68a30f9dcc77
Karl Waclawek <karl@waclawek.net>
2022-05-23 00:35:10 -0400

    AgentManager: removed custom root certificate from docker image and made it dynamic by installing it through run-docker.cmd.

--- EtwEvents.AgentManager/run-docker.cmd
+++ EtwEvents.AgentManager/run-docker.cmd
@@ -1,14 +1,30 @@
 pushd ..
+
+@echo off
+
+:: prompt for certificate password
+set /p certPwd=Server Certificate Password:
+cls
+
+:: start production container
+@echo Starting Container KdSoft_ETW_AgentManager
 docker run -dt -p 50301:80 -p 50300:443 ^
     --mount type=bind,src=c:/temp/etw-manager/keys,dst=/root/.aspnet/DataProtection-Keys/ ^
     --mount type=bind,src=c:/temp/etw-manager/certs,dst=/app/certs/,readonly ^
     -e "Kestrel:Endpoints:Https:Certificate:Path=/app/certs/agent-manager.kd-soft.net.p12" ^
-    -e "Kestrel:Endpoints:Https:Certificate:Password=_schroed" ^
+    -e "Kestrel:Endpoints:Https:Certificate:Password=%certPwd%" ^
     -e "ASPNETCORE_ENVIRONMENT=Production" ^
     --name "KdSoft_ETW_AgentManager" ^
     waclawek/kdsoft-etw-agent-manager:1.0
+::  if a network is specified, use the option --network=XXX
+
+:: clear certificate password
+set certPwd=
+
+:: install root certificate
+@echo Installing root certificate C:/Temp/etw-manager/certs/Kd-Soft.crt
+docker cp "C:/Temp/etw-manager/certs/Kd-Soft.crt" KdSoft_ETW_AgentManager:"/usr/local/share/ca-certificates"
+docker exec -dt -w "/usr/local/share/ca-certificates" KdSoft_ETW_AgentManager update-ca-certificates
+
 popd
-@echo off
-rem we could use -e "Kestrel:Endpoints:Https:Certificate:Password=xxxxx" instead if user secrets does not work
-rem or we could use docker secrets
-rem     --network=etw-net ^
+
