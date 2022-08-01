142262c98a81744b692930f72e3c408beb6de5aa
Karl Waclawek <karl@waclawek.net>
2022-08-01 16:36:36 -0400

    PushAgent: refresh SocketsHandlerCache with specific certioficates and not options, so we can be sure these certificates are used.,

--- EtwEvents.PushAgent/InfraStructure/SocketsHandlerCache.cs
+++ EtwEvents.PushAgent/InfraStructure/SocketsHandlerCache.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using Microsoft.Extensions.Options;
 
@@ -12,6 +13,7 @@ namespace KdSoft.EtwEvents.PushAgent
     class SocketsHandlerCache
     {
         readonly IOptionsMonitor<ControlOptions> _options;
+        X509Certificate2[] _overrideCertificates = new X509Certificate2[0];
 
         public SocketsHandlerCache(IOptionsMonitor<ControlOptions> options) {
             this._options = options;
@@ -19,15 +21,18 @@ namespace KdSoft.EtwEvents.PushAgent
         }
 
         SocketsHttpHandler CreateHandler() {
-            var clientCerts = Utils.GetClientCertificates(_options.CurrentValue.ClientCertificate);
-            if (clientCerts.Count == 0)
-                throw new ArgumentException("Cannot find a client certificate based on specified options.", nameof(_options.CurrentValue.ClientCertificate));
-            var handler = Utils.CreateHttpHandler(clientCerts.ToArray());
-            return handler;
+            if (_overrideCertificates.Length == 0) {
+                var clientCerts = Utils.GetClientCertificates(_options.CurrentValue.ClientCertificate);
+                if (clientCerts.Count == 0)
+                    throw new ArgumentException("Cannot find a client certificate based on specified options.", nameof(_options.CurrentValue.ClientCertificate));
+                return Utils.CreateHttpHandler(clientCerts.ToArray());
+            }
+            return Utils.CreateHttpHandler(_overrideCertificates);
         }
 
         int invalid = 0;
-        public bool Invalidate() {
+        public bool Refresh(params X509Certificate2[] overrideCertificates) {
+            _overrideCertificates = overrideCertificates ?? new X509Certificate2[0];
             return Interlocked.Exchange(ref invalid, 99) == 0;
         }
 
