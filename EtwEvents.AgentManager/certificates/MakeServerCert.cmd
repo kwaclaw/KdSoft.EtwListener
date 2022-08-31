55ea1e47066f34a3d5ab712491fb380b31a6b524
Karl Waclawek <karl@waclawek.net>
2022-08-31 19:32:15 -0400

    AgentManager: cosmetics on certificate scripts.

--- EtwEvents.AgentManager/certificates/MakeServerCert.cmd
+++ EtwEvents.AgentManager/certificates/MakeServerCert.cmd
@@ -1,18 +1,18 @@
 @echo off
 Setlocal enabledelayedexpansion
+:: switch to location of this script
+pushd "%~dp0"
 
 :: Prerequisite: OpenSSL >= 3.0 must be installed - see https://slproweb.com/products/Win32OpenSSL.html
 ::               or see https://kb.firedaemon.com/support/solutions/articles/4000121705
 
 :: Example: MakeServerCert.cmd -dns sales.my-company.com -email karl@kd-soft.net
 
-:: Modify for your scenario
+:: MODIFY FOR YOUR SCENARIO
 set ca_sign_file=Kd-Soft.crt
 set ca_key_file=Kd-Soft.key
 set base_distinguished_name=/C=CA/ST=ON/L=Oshawa/O=Kd-Soft
 
-:: switch to location of this script
-pushd "%~dp0"
 
 :: Common Name (CN) for server, also subjectAltName
 set -dns=
@@ -39,6 +39,7 @@ echo Command Line Parameters
 echo -dns=%-dns%
 echo -email=%-email%
 
+
 if ["%-dns%"] == [""] (set -dns=server.kd-soft.net)
 if ["%-email%"] == [""] (set -email=admin@kd-soft.net)
 
@@ -49,6 +50,7 @@ set dn=%base_distinguished_name%/%cn%%em%
 mkdir tmp 2>nul
 mkdir out 2>nul
 
+
 @echo generate server key
 openssl ecparam -out "tmp/server.key" -name secp384r1 -genkey
 if %ERRORLEVEL% NEQ 0 (Exit /b)
@@ -58,7 +60,7 @@ openssl req -new -key "tmp/server.key" -out "tmp/server.csr" -config "server.cnf
 if %ERRORLEVEL% NEQ 0 (Exit /b)
     
 @echo create and sign the server certificate    
-openssl x509 -req  -sha384 -days 398 -in "tmp/server.csr" -CA "%ca_sign_file%" -CAkey "%ca_key_file%" ^
+openssl x509 -req -sha384 -days 398 -in "tmp/server.csr" -CA "%ca_sign_file%" -CAkey "%ca_key_file%" ^
     -CAcreateserial -out "tmp/server.crt" -copy_extensions copy
 if %ERRORLEVEL% NEQ 0 (Exit /b)
     
