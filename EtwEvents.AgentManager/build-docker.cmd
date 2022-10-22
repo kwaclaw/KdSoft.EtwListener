47a28520fa6277ed80255e9a42e99505944bc98c
Karl Waclawek <karl@waclawek.net>
2022-10-22 16:57:04 -0400

    AgentManager: rebuild docker image.

--- EtwEvents.AgentManager/build-docker.cmd
+++ EtwEvents.AgentManager/build-docker.cmd
@@ -1,4 +1,4 @@
 pushd ..
-docker build --force-rm -f ./EtwEvents.AgentManager/Dockerfile -t waclawek/kdsoft-etw-agent-manager:1.1 --target final .
+docker build --force-rm -f ./EtwEvents.AgentManager/Dockerfile -t waclawek/kdsoft-etw-agent-manager:1.1.1 --target final .
 popd
 pause
