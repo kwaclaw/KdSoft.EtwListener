680737079831aabc139c89592d2c311cc120cac6
Karl Waclawek <karl@waclawek.net>
2023-12-23 17:26:40 -0500

    EtwEvents.PushAgent - cleanup in InstallService.ps1.

--- EtwEvents.PushAgent/deploy/InstallService.ps1
+++ EtwEvents.PushAgent/deploy/InstallService.ps1
@@ -139,10 +139,10 @@ function Import-Cert {
     )
 
     $role = $null
-    $cred = Get-Credential -UserName 'Installer' -Message ('Password for ' + $clientCertFile)
-    if ($cred) {
+    $certCred = Get-Credential -UserName 'Installer' -Message ('Password for ' + $clientCertFile)
+    if ($certCred) {
         try {
-            $clientCert = Import-PfxCertificate -FilePath $clientCertFile -CertStoreLocation $storeLocation -Password $cred.Password
+            $clientCert = Import-PfxCertificate -FilePath $clientCertFile -CertStoreLocation $storeLocation -Password $certCred.Password
             $subjectName = $clientCert.SubjectName.Decode(128)
             if ($subjectName -match 'OID\.2\.5\.4\.72=([^,]*)') { 
                 $role = $Matches[1]
@@ -299,9 +299,7 @@ if (!(Test-Path $targetDirPath)) {
 # LocalSystem account has all kinds of permissions already
 if ($cred.UserName -notlike '*\LocalSystem') {
     $acl = Get-Acl "$targetDirPath"
-    $aclRuleArgs = $cred.UserName, "Read,Write,ReadAndExecute", "ContainerInherit,ObjectInherit", "None", "Allow"
-    $accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($aclRuleArgs)
-    #$accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($cred.UserName, "Read,Write,ReadAndExecute", "ContainerInherit,ObjectInherit", "None", "Allow")
+    $accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($cred.UserName, "Read,Write,ReadAndExecute", "ContainerInherit,ObjectInherit", "None", "Allow")
     $acl.SetAccessRule($accessRule)
     $acl | Set-Acl "$targetDirPath"
 }
