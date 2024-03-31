c192d1d04e536e5f597c647dd5c1756f5ddb5990
Karl Waclawek <karl@waclawek.net>
2024-03-31 19:00:06 -0400

    EtwEvents.AgentCommand - improved remote install demo

--- EtwEvents.AgentCommand/RemoteInstallDemo.ps1
+++ EtwEvents.AgentCommand/RemoteInstallDemo.ps1
@@ -1,24 +1,28 @@
-﻿# copy installer to target host
-copy-item -path "<local directory>\EtwEvents.PushAgent.Setup.msi" -Destination "\\<remote host>\c$\windows\temp\EtwEvents.PushAgent.Setup.msi"
-
-# start remote management session, and if not already a domain admin, use admin credentials (admin on remote host)
+﻿# Create PowerShell session on remote host
 # Note: -UseSSL is only necessary for using WinRM with standalone hosts (and is hard to configure on the host)
-Enter-PSSession -ComputerName <remote host> [-Credential <admin user name>] [-UseSSL]
+$session = New-PSSession -ComputerName <remote host> [-Credential <admin user name>] [-UseSSL]
+
+# Copy installer to remote host
+Copy-Item -Path "<local directory>\EtwEvents.PushAgent.Setup.msi" -Destination -path "<remote directory>\EtwEvents.PushAgent.Setup.msi" -ToSession $session
 
 # from within that remote session install the agent
-msiexec /i c:\windows\temp\EtwEvents.PushAgent.Setup.msi /quiet /log c:\windows\temp\etw-agent.log
+Invoke-Command -Session $session -ScriptBlock { msiexec /i c:\temp\EtwEvents.PushAgent.Setup.msi /quiet /log c:\temp\etw-agent.log }
 
-# exit remote session, switch to directory where the agent command line tool can be found, then run the following commands
+# Exit remote session
+Remove-PSSession -Session $session
 
-# set control options (must be pre-configured in appsettings.json)
+# Switch to directory where the agent command line tool can be found, then run the following commands
+
+# Set control options (must be pre-configured in appsettings.json)
 KdSoft.EtwEvents.AgentCommand.exe --config="appsettings.json" --host <remote host> --cmd set-control
 
-#  install new client certificate, the site name must be unique (will be created, email is optional)
+# Install new client certificate, the site name must be unique (will be created, email is optional)
 KdSoft.EtwEvents.AgentCommand.exe --host <remote host> --cmd new-cert --site-name test-site-1 [--site-email <contact email>]
 
-#  set site options (configure and download from agent manager - "Export as Command")
+# Set site options (configure and download from agent manager - "Export as Command")
+# Note: the options file does not have to include a complete set of options, one can, for instance, just set the event sinks
 KdSoft.EtwEvents.AgentCommand.exe --host <remote host> --cmd set-options --site-options="D:\\Downloads\\TestSite1.options-command.json\"
 
-# start the agent
+# Start the agent
 KdSoft.EtwEvents.AgentCommand.exe --host <remote host> --cmd start
 
