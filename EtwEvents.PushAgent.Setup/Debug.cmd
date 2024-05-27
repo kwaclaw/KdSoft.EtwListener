78885f08de06d4883b462b309876a2a70ba0cb60
Karl Waclawek <karl@waclawek.net>
2024-05-26 23:15:29 -0400

    EtwEvents.PushAgent.Setup - CustomAction did not run Before InstallInitialize. Suppress generation of registry entries.

--- EtwEvents.PushAgent.Setup/Debug.cmd
+++ EtwEvents.PushAgent.Setup/Debug.cmd
@@ -1,1 +1,5 @@
-msiexec /i bin\x64\Debug\en-US\EtwEvents.PushAgent.Setup.msi /l!*vx install.log
+:: Very detailed logging
+:: msiexec /i bin\x64\Debug\en-US\EtwEvents.PushAgent.Setup.msi /l!*vx install.log
+
+:: Default logging
+msiexec /i bin\x64\Debug\en-US\EtwEvents.PushAgent.Setup.msi /l install.log
